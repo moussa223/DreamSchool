5a51c8d8473ceac340322760f83a0502405b874b
moussa223 <mm6824963@gmail.com>
2023-08-23 12:11:59 +0200

    Implémentation du composant calendrier affichant les jours et mois de l année

--- src/app/calendrier/calendrier.component.ts
+++ src/app/calendrier/calendrier.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendrier.component.css']
 })
 export class CalendrierComponent implements OnInit {
+  currentMonth: any;
+  days: string[];
+  calendarDays: any;
 
-  constructor() { }
+  constructor() {
+    this.currentMonth = new Date(); // Month from your API or logic
+    this.days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; // Days of the week
+    this.generateCalendar();
+  }
+  generateCalendar() {
+    this.calendarDays = [];
+    const startDay = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth(), 1).getDay();
+    const totalDaysInMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() + 1, 0).getDate();
 
+    for (let i = 1; i <= totalDaysInMonth; i++) {
+      const day = (startDay + i - 1) % 7; // Calculate the day index (0 - 6)
+      this.calendarDays.push({ day: i, dayOfWeek: this.days[day] });
+    }
+  }
   ngOnInit(): void {
   }
+
+  prevMonth() {
+    this.currentMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() - 1);
+    this.generateCalendar();
+  }
+
+  nextMonth() {
+    this.currentMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() + 1);
+    this.generateCalendar();
+  }
+
+
 
 }
